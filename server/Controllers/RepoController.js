a691613873d8b7f5fa0484a427563915da6136cb
developingright <developingright@gmail.com>
2024-09-03 00:24:10 +0530

    fixed repo creating bug

--- server/Controllers/RepoController.js
+++ server/Controllers/RepoController.js
@@ -23,7 +23,8 @@ router.post("/create", async (req, res) => {
     const { repo_url, UserId } = req.body;
 
     const repo = await RepoModel.findOne({ repo_url });
-    if (repo.UserId == UserId) {
+    console.log(repo);
+    if (repo && repo.UserId == UserId) {
       return res.json({ message: "repository already exists !" });
     }
     const data = repo_url.slice(19);
